f21e1aa67f9598081d4ae9449137402b31bdb06d
thossain30 <54952959+thossain30@users.noreply.github.com>
2023-03-04 19:42:01 -0500

    Began adding JS
    - Linked Jquery
    - Added clear history button
    - Attached click events to buttons

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+let searchBtn = $("#search-button");
+let clearBtn = $("#clear-history");
+
+// Will run necessary events when these buttons are clicked
+searchBtn.click(function() {
+    console.log("searched");
+});
+
+clearBtn.click(function() {
+    console.log("cleared");
+});
+// API Key
+let APIKey = "455419fbacd85967af11809073040f3e";
