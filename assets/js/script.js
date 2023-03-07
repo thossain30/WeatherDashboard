ef7ab71315dc424b8c201c27e6afa7f1ff509609
thossain30 <54952959+thossain30@users.noreply.github.com>
2023-03-06 23:47:34 -0500

    Began working on 5 day forecast feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,7 @@ let humidity = $("#humidity");
 let windSpeed = $("#wind-speed");
 let uvIndex = $("#uv-index");
 
+
 // API Key
 let APIKey = "455419fbacd85967af11809073040f3e";
 
@@ -26,17 +27,48 @@ clearBtn.click(function() {
     console.log("cleared");
 });
 
+// Function to handle displaying Current Day
 function currentWeather(city) {
+    let currentDay = dayjs().format("MM/DD/YYYY");
     // URL for a request
-    let requestURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
+    "api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}"
+    let requestURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${APIKey}`;
     fetch(requestURL).
         then(function(response) {
             return(response.json());
         }).then(function(data) {
-            console.log(data);
-            curCity.text(data.name);
-            let tempF = Math.round(((data.main.temp - 273.15) * 1.80 + 32));
-            temperature.text(" " + tempF + " F");
+            var weathericon= data.weather[0].icon;
+            var iconurl="https://openweathermap.org/img/wn/"+weathericon +"@2x.png";
             curCity.css("display", "block");
+            curCity.html(data.name + " (" + currentDay + ") " + "<img src="+iconurl+"></img>");
+            temperature.text(" " + data.main.temp + " °F");
+            humidity.text(" " + data.main.humidity + "%");
+            windSpeed.text(" " + data.wind.speed + " m/s");
+            forecast(data.id);
+        })
+}
+
+// Function to handle displaying forecast for next 5 days
+function forecast(cityid) {
+    let dayOver = false;
+    let forecastURL = `api.openweathermap.org/data/2.5/forecast?id=${cityid}&APPID=${APIKey}`
+    fetch(forecastURL).
+        then(function(response) {
+            return response.json();
+        }).then(function(data) {
+            for (i=0;i<5;i++){
+                // var date= new Date((response.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
+                let date = dayjs((data.list[((i+1)*8)-1].dt)*1000).format("MM/DD/YYYY");
+                console.log("date: " + date);
+                var iconcode= data.list[((i+1)*8)-1].weather[0].icon;
+                var iconurl="https://openweathermap.org/img/wn/"+iconcode+".png";
+                var temp= data.list[((i+1)*8)-1].main.temp;
+                var humidity= data.list[((i+1)*8)-1].main.humidity;
+            
+                $("#futDate"+i).html(date);
+                $("#futImg"+i).html("<img src="+iconurl+">");
+                $("#futTemp"+i).html(temp+"&#8457");
+                $("#futHumidity"+i).html(humidity+"%");
+            }
         })
 }
